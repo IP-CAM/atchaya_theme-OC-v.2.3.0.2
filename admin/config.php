058b4f967fe1c0309c74df36ca53af89f3c26f3f
saravana02 <36338737+saravana02@users.noreply.github.com>
2018-02-12 00:01:30 +0530

    Initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/atchaya_theme/admin/');
+define('HTTP_CATALOG', 'http://localhost/atchaya_theme/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/atchaya_theme/admin/');
+define('HTTPS_CATALOG', 'http://localhost/atchaya_theme/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/xampp/htdocs/atchaya_theme/admin/');
+define('DIR_SYSTEM', 'F:/xampp/htdocs/atchaya_theme/system/');
+define('DIR_IMAGE', 'F:/xampp/htdocs/atchaya_theme/image/');
+define('DIR_LANGUAGE', 'F:/xampp/htdocs/atchaya_theme/admin/language/');
+define('DIR_TEMPLATE', 'F:/xampp/htdocs/atchaya_theme/admin/view/template/');
+define('DIR_CONFIG', 'F:/xampp/htdocs/atchaya_theme/system/config/');
+define('DIR_CACHE', 'F:/xampp/htdocs/atchaya_theme/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/xampp/htdocs/atchaya_theme/system/storage/download/');
+define('DIR_LOGS', 'F:/xampp/htdocs/atchaya_theme/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/xampp/htdocs/atchaya_theme/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/xampp/htdocs/atchaya_theme/system/storage/upload/');
+define('DIR_CATALOG', 'F:/xampp/htdocs/atchaya_theme/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'opc_atchaya_theme');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
