f60b908dc939a7b675dde62514ad01cc6188197e
saravana02 <36338737+saravana02@users.noreply.github.com>
2018-02-14 01:18:27 +0530

    Video Blog menu admin panel

---
+++ admin/language/en-gb/blog/videolist.php
@@ -0,0 +1,31 @@
+<?php
+// Heading
+$_['heading_title']          = 'Blog Videos List';
+
+// Text
+$_['text_success']           = 'Success: You have modified Blog Videos list!';
+$_['text_list']              = 'Blog Videos Lists';
+$_['text_add']               = 'Add Blog Video List';
+$_['text_edit']              = 'Edit Blog Video List';
+$_['text_extension']         = 'Extensions';
+$_['text_blog']              = 'Blog';
+
+// Column
+$_['column_name']            = 'Blog Video List Name';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Blog Videos List Name';
+$_['entry_Blog Video_list']     = 'Blog Videos';
+$_['entry_status']           = 'Status';
+
+// Help
+$_['help_category']          = '(Autocomplete)';
+
+// Error
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify Blog Videos!';
+$_['error_name']             = 'Blog Video Name must be greater than 3 and less than 255 characters!';
+$_['error_meta_title']       = 'Meta Title must be greater than 3 and less than 255 characters!';
+$_['error_keyword']          = 'SEO keyword already in use!';
