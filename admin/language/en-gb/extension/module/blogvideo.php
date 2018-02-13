572e3c1d8dafb4fd14d0cdc00909117afba5580a
saravana02 <36338737+saravana02@users.noreply.github.com>
2018-02-14 00:11:21 +0530

    blog video module

--- admin/language/en-gb/extension/module/blogvideo.php
+++ admin/language/en-gb/extension/module/blogvideo.php
@@ -5,10 +5,12 @@ $_['heading_title']    = 'Blog Video';
 // Text
 $_['text_extension']   = 'Extensions';
 $_['text_success']     = 'Success: You have modified account module!';
-$_['text_edit']        = 'Edit Account Module';
+$_['text_edit']        = 'Blog Video';
 
 // Entry
 $_['entry_status']     = 'Status';
+$_['entry_name']       = 'Video Url';
+$_['button_save']      = 'save';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify account module!';
