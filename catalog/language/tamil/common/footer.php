ed450707d9246eaf3111090f9e4a8b5c4f4a5dc7
gB <dongobinath@gmail.com>
2018-04-11 21:48:43 +0530

    Gobi

---
+++ catalog/language/tamil/common/footer.php
@@ -0,0 +1,17 @@
+<?php
+// Text
+$_['text_information']  = 'தகவல்';
+$_['text_service']      = 'வாடிக்கையாளர் சேவை';
+$_['text_extra']        = 'கூடுதல்';
+$_['text_contact']      = 'எங்களை தொடர்பு';
+$_['text_return']       = 'திரும்ப';
+$_['text_sitemap']      = 'தள வரைபடம்';
+$_['text_manufacturer'] = 'Brands';
+$_['text_voucher']      = 'Gift Certificates';
+$_['text_affiliate']    = 'Affiliates';
+$_['text_special']      = 'சிறப்புகள்';
+$_['text_account']      = 'என் கணக்கு';
+$_['text_order']        = 'ஆர்டர் வரலாறு';
+$_['text_wishlist']     = 'பட்டியல் விரும்புகிறேன்';
+$_['text_newsletter']   = 'Newsletter';
+$_['text_powered']      = 'Powered By <a href="http://www.opencart.com">OpenCart</a><br /> %s &copy; %s';
